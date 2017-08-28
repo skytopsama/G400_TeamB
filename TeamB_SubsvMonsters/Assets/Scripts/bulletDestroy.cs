39e6b416e4499f2ad50b425dab31a7c341be47f4
Aaron Griffin <aasgriff@indiana.edu>
2017-08-28 12:21:57 -0400

    Implemented bullet pool and basic shooting from player. There is a slight delay between button down and the first projectile that fires.

---
+++ TeamB_SubsvMonsters/Assets/Scripts/bulletDestroy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bulletDestroy : MonoBehaviour {
+	public int speed;
+	public float lifeTime;
+	//public int power;
+
+
+	// Use this for initialization
+	void OnEnable () {
+		GetComponent<Rigidbody2D>().velocity = transform.up.normalized * speed;
+		Invoke ("Destroy", lifeTime);
+	}
+
+	void Destroy(){
+		Debug.Log ("bullet destroyed");
+		gameObject.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void OnDisable () {
+		CancelInvoke ();
+	}
+}
